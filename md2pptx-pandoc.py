bd66d39bb2dfdaad5a450b2b6f75fc4ae40b9012
HlexNC <hlexhelftd@gmail.com>
2023-10-02 17:27:57 +0200

    Create md2pptx-pandoc.py

---
+++ md2pptx-pandoc.py
@@ -0,0 +1,27 @@
+import pypandoc
+import datetime
+import os
+
+# Directory to save the converted files
+OUTPUT_DIR = 'converted_files'
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+
+# Converts markdown string to a pptx file and returns the filename
+def md2pptx(md):
+    date = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    filename = f'markdown-{date}.pptx'
+    output_path = os.path.join(OUTPUT_DIR, filename)
+    pypandoc.convert_text(md, 'pptx', format='md', outputfile=output_path)
+    return filename
+
+
+md = """
+# This is a heading
+## This is a subheading
+### This is a subsubheading
+"""
+
+filename = md2pptx(md)
+print(filename)
