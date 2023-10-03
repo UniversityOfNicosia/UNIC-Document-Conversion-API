1b89ed06c2f436c44ec7f71cc783c5b5e6d872be
HlexNC <hlexhelftd@gmail.com>
2023-10-03 10:56:34 +0200

    Update: Refactor conversion API and enhance .gitignore
    Added changes in `md2word-pandoc.py`:
    - Migrate from Flask to FastAPI.
    - Introduce `MarkdownRequest` class for Pydantic validation.
    - Enhance `md2word` function with type hints.
    - Update `/md2word` endpoint to utilize FastAPI's decorators and validation.
    - Add `/download/{filename}` endpoint for serving converted files.
    - Implement FastAPI's HTTP exceptions for error handling.
    - Utilize `uvicorn` for serving the FastAPI app.
    
    Added changes in `.gitignore`:
    - Ignore Python cache files by adding `__pycache__/`.
    
    The refactor to FastAPI offers modern API features, auto OpenAPI documentation, and request validation.

--- md2word-pandoc.py
+++ md2word-pandoc.py
@@ -1,39 +1,42 @@
-from flask import Flask, request, jsonify, send_from_directory
+from fastapi import FastAPI, HTTPException, UploadFile, Form, Depends
+from fastapi.responses import JSONResponse, FileResponse
+from pydantic import BaseModel
 import pypandoc
 import os
 import datetime
 
-app = Flask(__name__)
+app = FastAPI()
 
 # Directory to save the converted files
 OUTPUT_DIR = 'converted_files'
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
+class MarkdownRequest(BaseModel):
+    md: str
+
 
 # Converts markdown string to a docx file and returns the filename
-def md2word(md):
+def md2word(md: str) -> str:
     date = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     filename = f'markdown-{date}.docx'
     output_path = os.path.join(OUTPUT_DIR, filename)
     pypandoc.convert_text(md, 'docx', format='md', outputfile=output_path)
     return filename
 
-
-@app.route('/md2word', methods=['POST'])
-def convert_md2word():
+@app.post("/md2word")
+def convert_md2word(request: MarkdownRequest):
     try:
-        md = request.json['md']
-        filename = md2word(md)
-        return jsonify({'message': 'Conversion successful!', 'filename': filename})
+        filename = md2word(request.md)
+        return {"message": "Conversion successful!", "filename": filename, "result": request.md}
     except Exception as e:
-        return jsonify({'error': str(e)})
+        raise HTTPException(status_code=400, detail=str(e))
 
-
-@app.route('/download/<filename>', methods=['GET'])
-def download_file(filename):
-    return send_from_directory(OUTPUT_DIR, filename, as_attachment=True)
+@app.get("/download/{filename}")
+def download_file(filename: str):
+    return FileResponse(os.path.join(OUTPUT_DIR, filename), headers={"Content-Disposition": f"attachment; filename={filename}"})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
