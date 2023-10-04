dab6e77420a2b2810392396776370423f38fd5c1
HlexNC <hlexhelftd@gmail.com>
2023-10-04 17:38:02 +0200

    Update: Enhance Markdown-to-DOCX conversion in `md2word-pandoc.py`
    Added changes in `md2word-pandoc.py`:
    - Organized and grouped import statements for better readability.
    - Added debugging print statements for logging the received markdown content and the content passed to pypandoc.
    - Integrated the use of `codecs` to decode escaped sequences such as `\\n`, `\\t`, and `\\r` in the markdown content.
    - Modified the download file endpoint to be more readable by separating path and headers.
    
    Addressed issue in the Markdown-to-DOCX conversion endpoint where escaped sequences (like \\n, \\t, and \\r) were not being properly decoded. The endpoint now correctly handles and decodes these sequences, ensuring accurate document formatting in the resulting .docx file.

--- md2word-pandoc.py
+++ md2word-pandoc.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, HTTPException, UploadFile, Form, Depends
-from fastapi.responses import JSONResponse, FileResponse
-from pydantic import BaseModel
-import pypandoc
-import os
 import datetime
+import os
+
+import codecs
+import pypandoc
+from fastapi import Depends, FastAPI, Form, HTTPException, UploadFile
+from fastapi.responses import FileResponse, JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -12,29 +14,37 @@ OUTPUT_DIR = 'converted_files'
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
+
 class MarkdownRequest(BaseModel):
     md: str
 
 
 # Converts markdown string to a docx file and returns the filename
 def md2word(md: str) -> str:
+    print(f"Content passed to pypandoc:\n{md}")
     date = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     filename = f'markdown-{date}.docx'
     output_path = os.path.join(OUTPUT_DIR, filename)
     pypandoc.convert_text(md, 'docx', format='md', outputfile=output_path)
     return filename
 
+
 @app.post("/md2word")
 def convert_md2word(request: MarkdownRequest):
+    request.md = codecs.decode(request.md, 'unicode_escape')
+    print(f"Received markdown content: {request.md}")
     try:
         filename = md2word(request.md)
         return {"message": "Conversion successful!", "filename": filename, "result": request.md}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+
 @app.get("/download/{filename}")
 def download_file(filename: str):
-    return FileResponse(os.path.join(OUTPUT_DIR, filename), headers={"Content-Disposition": f"attachment; filename={filename}"})
+    path = os.path.join(OUTPUT_DIR, filename)
+    headers = {"Content-Disposition": f"attachment; filename={filename}"}
+    return FileResponse(path, headers=headers)
 
 
 if __name__ == '__main__':
