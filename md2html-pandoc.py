78b217e1b6724287390896c00274ff8b631d5e57
HlexNC <hlexhelftd@gmail.com>
2023-10-05 17:11:08 +0200

    Update: Enhancements to the Markdown-to-HTML conversion in `md2html-pandoc.py`
    Added changes in `md2html-pandoc.py`:
    - Transitioned to using the `pathlib` library for directory and file path operations.
    - Replaced traditional `os` methods with `Path.mkdir` to create directories, ensuring enhanced cross-platform compatibility.
    - Introduced the `MarkdownRequest` class to model incoming Markdown content for conversion.
    - Adjusted the `md2html` function to accept a string argument and return a string representing the filename.
    - Integrated a POST endpoint `/md2html` to handle Markdown-to-HTML conversions, including decoding of escaped sequences.
    - Incorporated a GET endpoint `/download/{filename}` to enable downloading of the converted HTML file.
    - Reorganized and structured the imports for improved clarity.
    
    These modifications bolster the functionality of the Markdown-to-HTML conversion process, delivering a more intuitive API structure and leveraging modern Python libraries for refined code quality.

--- md2html-pandoc.py
+++ md2html-pandoc.py
@@ -1,17 +1,49 @@
-import pypandoc
 import datetime
-import os
+import codecs
+import pypandoc
+from fastapi import Depends, FastAPI, Form, HTTPException, UploadFile
+from fastapi.responses import FileResponse, JSONResponse
+from pydantic import BaseModel
+from pathlib import Path
+
+app = FastAPI()
 
 # Directory to save the converted files
 OUTPUT_DIR = 'converted_files'
-if not os.path.exists(OUTPUT_DIR):
-    os.makedirs(OUTPUT_DIR)
+Path(OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
+
+
+class MarkdownRequest(BaseModel):
+    md: str
 
 
 # Converts markdown string to an html file and returns the filename
 def md2html(md):
     date = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     filename = f'markdown-{date}.html'
-    output_path = os.path.join(OUTPUT_DIR, filename)
+    output_path = Path(OUTPUT_DIR) / filename
     pypandoc.convert_text(md, 'html', format='md', outputfile=output_path)
     return filename
+
+
+@app.post("/md2html")
+def convert_md2html(request: MarkdownRequest):
+    request.md = codecs.decode(request.md, 'unicode_escape')
+    print(f"Received markdown content: {request.md}")
+    try:
+        filename = md2html(request.md)
+        return {"message": "Conversion successful!", "filename": filename, "result": request.md}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+
+@app.get("/download/{filename}")
+def download_file(filename: str):
+    path = Path(OUTPUT_DIR) / filename
+    headers = {"Content-Disposition": f"attachment; filename={filename}"}
+    return FileResponse(path, headers=headers)
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
